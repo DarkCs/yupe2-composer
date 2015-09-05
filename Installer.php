ba567419d461b13abe19ce5ee0f4afe90d8ddc9e
Dark_Cs <darkcs2@gmail.com>
2015-09-05 17:47:12 +0300

    fix

--- Installer.php
+++ Installer.php
@@ -3,11 +3,8 @@
 namespace yupe\composer;
 
 use Composer\Installer\LibraryInstaller;
-use Composer\IO\IOInterface;
-use Composer\Composer;
-use Composer\Package\PackageInterface;
 
-class PluginInstaller extends LibraryInstaller
+class Installer extends LibraryInstaller
 {
     public function supports($packageType)
     {
